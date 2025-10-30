ed71865e22e3af156fdfd083b14a75a38167cfa7
Jozef Mlich <jozef.mlich@greycortex.com>
2025-10-30 07:20:54 +0100

    Update translations

--- translations/harbour-confla-cs_CZ.ts
+++ translations/harbour-confla-cs_CZ.ts
@@ -4,36 +4,44 @@
 <context>
     <name></name>
     <message id="application-title">
+        <location filename="../qml/main.qml" line="12"/>
         <source>Confla</source>
         <oldsource>DevConf</oldsource>
         <translation>Confla</translation>
     </message>
     <message id="section-header-about">
+        <location filename="../qml/ConferenceDetailPage.qml" line="32"/>
         <source>About</source>
         <translation>O konferenci</translation>
     </message>
     <message id="section-about-places">
+        <location filename="../qml/ConferenceDetailPage.qml" line="103"/>
         <source>Important Places</source>
         <oldsource>Places</oldsource>
         <translation>Důležitá místa</translation>
     </message>
     <message id="section-header-schedule">
+        <location filename="../qml/ConferenceDetailPage.qml" line="121"/>
         <source>Schedule</source>
         <translation>Program</translation>
     </message>
     <message id="section-favorites">
+        <location filename="../qml/ConferenceDetailPage.qml" line="162"/>
         <source>Favorites</source>
         <translation>Oblíbené</translation>
     </message>
     <message id="section-header-upcoming-talks">
+        <location filename="../qml/ConferenceDetailPage.qml" line="180"/>
         <source>Upcoming talks</source>
         <translation>Nadcházející přednášky</translation>
     </message>
     <message id="section-header-news">
+        <location filename="../qml/ConferenceDetailPage.qml" line="225"/>
         <source>News</source>
         <translation>Novinky</translation>
     </message>
     <message id="format-days-countdown" numerus="yes">
+        <location filename="../qml/functions.js" line="31"/>
         <source>%n days</source>
         <translation>
             <numerusform>%n den</numerusform>
@@ -42,94 +50,117 @@
         </translation>
     </message>
     <message id="dayofweek-sunday">
+        <location filename="../qml/functions.js" line="44"/>
         <source>Sunday</source>
         <translation>Neděle</translation>
     </message>
     <message id="dayofweek-monday">
+        <location filename="../qml/functions.js" line="46"/>
         <source>Monday</source>
         <translation>Pondělí</translation>
     </message>
     <message id="dayofweek-tuesday">
+        <location filename="../qml/functions.js" line="48"/>
         <source>Tuesday</source>
         <translation>Úterý</translation>
     </message>
     <message id="dayofweek-wednesday">
+        <location filename="../qml/functions.js" line="50"/>
         <source>Wednesday</source>
         <translation>Středa</translation>
     </message>
     <message id="dayofweek-thursday">
+        <location filename="../qml/functions.js" line="52"/>
         <source>Thursday</source>
         <translation>Čtvrtek</translation>
     </message>
     <message id="dayofweek-friday">
+        <location filename="../qml/functions.js" line="54"/>
         <source>Friday</source>
         <translation>Pátek</translation>
     </message>
     <message id="dayofweek-saturday">
+        <location filename="../qml/functions.js" line="56"/>
         <source>Saturday</source>
         <translation>Sobota</translation>
     </message>
     <message id="daysofweek-today">
+        <location filename="../qml/functions.js" line="75"/>
         <source>Today</source>
         <translation>Dnes</translation>
     </message>
     <message id="daysofweek-yesterday">
+        <location filename="../qml/functions.js" line="78"/>
         <source>Yesterday</source>
         <translation>Včera</translation>
     </message>
     <message id="map-title">
+        <location filename="../qml/MapPage.qml" line="24"/>
         <source>Places</source>
         <translation>Místa</translation>
     </message>
     <message id="image-status-ready">
+        <location filename="../qml/PinchMap.qml" line="308"/>
         <source>Ready</source>
         <translation>Připraveno</translation>
     </message>
     <message id="image-status-not-set">
+        <location filename="../qml/PinchMap.qml" line="310"/>
         <source>Not Set</source>
         <translation>Není nastaveno</translation>
     </message>
     <message id="image-status-error">
+        <location filename="../qml/PinchMap.qml" line="312"/>
         <source>Error</source>
         <translation>Chyba</translation>
     </message>
     <message id="image-status-loading">
+        <location filename="../qml/PinchMap.qml" line="314"/>
         <source>Loading ...</source>
         <translation>Načítá se ...</translation>
     </message>
     <message id="image-status-unknown">
+        <location filename="../qml/PinchMap.qml" line="316"/>
         <source>Unknown</source>
         <translation>Neznámo</translation>
     </message>
     <message id="place-detail-header-place">
+        <location filename="../qml/PlaceDetailPage.qml" line="40"/>
         <source>Place</source>
         <translation>Místo</translation>
     </message>
     <message id="show-on-map">
+        <location filename="../qml/PlaceDetailPage.qml" line="107"/>
         <source>Show on Map</source>
         <translation>Ukázat na mapě</translation>
     </message>
     <message id="navigate-to">
+        <location filename="../qml/PlaceDetailPage.qml" line="124"/>
         <source>Navigate to</source>
         <translation>Navigovat do</translation>
     </message>
     <message id="schedule-page-header-favorites">
+        <location filename="../qml/SchedulePage.qml" line="160"/>
         <source>Favorites</source>
         <translation>Oblíbené</translation>
     </message>
     <message id="remove-from-favorites">
+        <location filename="../qml/EventDetailPage.qml" line="189"/>
         <source>Remove from favorites</source>
         <translation>Odstranit z oblíbených</translation>
     </message>
     <message id="add-to-favorites">
+        <location filename="../qml/EventDetailPage.qml" line="192"/>
         <source>Add to favorites</source>
         <translation>Přidat do oblíbených</translation>
     </message>
     <message id="open-feedback-form">
+        <location filename="../qml/EventDetailPage.qml" line="209"/>
         <source>Leave your feedback here</source>
         <translation>Dejte nám zpětnou vazbu</translation>
     </message>
     <message id="conference-list-page-list-is-empty">
+        <location filename="../qml/ConferenceListPage.qml" line="69"/>
         <source>List of conferences is empty</source>
         <oldsource>List of conferences is not present in cache</oldsource>
         <translation>Seznam konferencí je prázdný</translation>
@@ -138,6 +169,8 @@
 <context>
     <name>DownloadDataMenuItem</name>
     <message>
+        <location filename="../qml/components/DownloadDataMenuItem.qml" line="6"/>
+        <location filename="../qml/components/DownloadDataMenuItem.qml" line="6"/>
         <source>Download Data</source>
         <translation type="unfinished"></translation>
     </message>
--- translations/harbour-confla-en_US.ts
+++ translations/harbour-confla-en_US.ts
@@ -4,36 +4,44 @@
 <context>
     <name></name>
     <message id="section-header-about">
+        <location filename="../qml/ConferenceDetailPage.qml" line="32"/>
         <source>About</source>
         <translation>About</translation>
     </message>
     <message id="section-about-places">
+        <location filename="../qml/ConferenceDetailPage.qml" line="103"/>
         <source>Important Places</source>
         <oldsource>Places</oldsource>
         <translation>Important Places</translation>
     </message>
     <message id="section-header-schedule">
+        <location filename="../qml/ConferenceDetailPage.qml" line="121"/>
         <source>Schedule</source>
         <translation>Schedule</translation>
     </message>
     <message id="section-favorites">
+        <location filename="../qml/ConferenceDetailPage.qml" line="162"/>
         <source>Favorites</source>
         <translation>Favorites</translation>
     </message>
     <message id="section-header-upcoming-talks">
+        <location filename="../qml/ConferenceDetailPage.qml" line="180"/>
         <source>Upcoming talks</source>
         <translation>Upcoming talks</translation>
     </message>
     <message id="section-header-news">
+        <location filename="../qml/ConferenceDetailPage.qml" line="225"/>
         <source>News</source>
         <translation>News</translation>
     </message>
     <message id="application-title">
+        <location filename="../qml/main.qml" line="12"/>
         <source>Confla</source>
         <oldsource>DevConf</oldsource>
         <translation>Confla</translation>
     </message>
     <message id="format-days-countdown" numerus="yes">
+        <location filename="../qml/functions.js" line="31"/>
         <source>%n days</source>
         <translation>
             <numerusform>%n day</numerusform>
@@ -41,94 +49,117 @@
         </translation>
     </message>
     <message id="dayofweek-sunday">
+        <location filename="../qml/functions.js" line="44"/>
         <source>Sunday</source>
         <translation>Sunday</translation>
     </message>
     <message id="dayofweek-monday">
+        <location filename="../qml/functions.js" line="46"/>
         <source>Monday</source>
         <translation>Monday</translation>
     </message>
     <message id="dayofweek-tuesday">
+        <location filename="../qml/functions.js" line="48"/>
         <source>Tuesday</source>
         <translation>Tuesday</translation>
     </message>
     <message id="dayofweek-wednesday">
+        <location filename="../qml/functions.js" line="50"/>
         <source>Wednesday</source>
         <translation>Wednesday</translation>
     </message>
     <message id="dayofweek-thursday">
+        <location filename="../qml/functions.js" line="52"/>
         <source>Thursday</source>
         <translation>Thursday</translation>
     </message>
     <message id="dayofweek-friday">
+        <location filename="../qml/functions.js" line="54"/>
         <source>Friday</source>
         <translation>Friday</translation>
     </message>
     <message id="dayofweek-saturday">
+        <location filename="../qml/functions.js" line="56"/>
         <source>Saturday</source>
         <translation>Saturday</translation>
     </message>
     <message id="daysofweek-today">
+        <location filename="../qml/functions.js" line="75"/>
         <source>Today</source>
         <translation>Today</translation>
     </message>
     <message id="daysofweek-yesterday">
+        <location filename="../qml/functions.js" line="78"/>
         <source>Yesterday</source>
         <translation>Yesterday</translation>
     </message>
     <message id="map-title">
+        <location filename="../qml/MapPage.qml" line="24"/>
         <source>Places</source>
         <translation>Places</translation>
     </message>
     <message id="image-status-ready">
+        <location filename="../qml/PinchMap.qml" line="308"/>
         <source>Ready</source>
         <translation>Ready</translation>
     </message>
     <message id="image-status-not-set">
+        <location filename="../qml/PinchMap.qml" line="310"/>
         <source>Not Set</source>
         <translation>Not Set</translation>
     </message>
     <message id="image-status-error">
+        <location filename="../qml/PinchMap.qml" line="312"/>
         <source>Error</source>
         <translation>Error</translation>
     </message>
     <message id="image-status-loading">
+        <location filename="../qml/PinchMap.qml" line="314"/>
         <source>Loading ...</source>
         <translation>Loading ...</translation>
     </message>
     <message id="image-status-unknown">
+        <location filename="../qml/PinchMap.qml" line="316"/>
         <source>Unknown</source>
         <translation>Unknown</translation>
     </message>
     <message id="place-detail-header-place">
+        <location filename="../qml/PlaceDetailPage.qml" line="40"/>
         <source>Place</source>
         <translation>Place</translation>
     </message>
     <message id="show-on-map">
+        <location filename="../qml/PlaceDetailPage.qml" line="107"/>
         <source>Show on Map</source>
         <translation>Show on Map</translation>
     </message>
     <message id="navigate-to">
+        <location filename="../qml/PlaceDetailPage.qml" line="124"/>
         <source>Navigate to</source>
         <translation>Navigate to</translation>
     </message>
     <message id="schedule-page-header-favorites">
+        <location filename="../qml/SchedulePage.qml" line="160"/>
         <source>Favorites</source>
         <translation>Favorites</translation>
     </message>
     <message id="remove-from-favorites">
+        <location filename="../qml/EventDetailPage.qml" line="189"/>
         <source>Remove from favorites</source>
         <translation>Remove from favorites</translation>
     </message>
     <message id="add-to-favorites">
+        <location filename="../qml/EventDetailPage.qml" line="192"/>
         <source>Add to favorites</source>
         <translation>Add to favorites</translation>
     </message>
     <message id="open-feedback-form">
+        <location filename="../qml/EventDetailPage.qml" line="209"/>
         <source>Leave your feedback here</source>
         <translation>Leave your feedback here</translation>
     </message>
     <message id="conference-list-page-list-is-empty">
+        <location filename="../qml/ConferenceListPage.qml" line="69"/>
         <source>List of conferences is empty</source>
         <oldsource>List of conferences is not present in cache</oldsource>
         <translation>List of conferences is empty</translation>
@@ -137,6 +168,8 @@
 <context>
     <name>DownloadDataMenuItem</name>
     <message>
+        <location filename="../qml/components/DownloadDataMenuItem.qml" line="6"/>
+        <location filename="../qml/components/DownloadDataMenuItem.qml" line="6"/>
         <source>Download Data</source>
         <translation type="unfinished"></translation>
     </message>
